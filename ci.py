cc56155773bc89011ab9d5d37bb63113c0ca2d9b
ghltbm <fairzlq@hotmail.com>
2025-12-06 21:16:50 +0800

    sign all

--- ci.py
+++ ci.py
@@ -10,7 +10,7 @@ from pathlib import Path
 
 # Configurable options
 
-IDENTITY = "OpenCore Legacy Patcher Software Signing"
+IDENTITY = "Hackdoc Signing"
 TARGET_DIR = Path("Universal-Binaries")
 UNUSED = [
     # Mojave and Catalina non-Metal Patches
